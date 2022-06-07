6d6f21a3c603fe2c160dde1a09fbf441cb5ec796
Dave-361 <davidoladeji673@gmail.com>
2022-06-06 19:33:30 -0700

    all possible combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Program that print all possible different combinations of two digits
+ *
+ * Return: Always Success
+ */
+int main(void)
+{
+	int i = 48;
+	int e = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}
